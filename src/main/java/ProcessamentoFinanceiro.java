608d556b1d73c4fd586bc3fb80a1664701cba185
vinicius lima <vinicius.limalveslinux@gmail.com>
2026-05-11 08:41:33 -0300

    Melhoria no ProcessamentoFinanceiro

--- src/main/java/ProcessamentoFinanceiro.java
+++ src/main/java/ProcessamentoFinanceiro.java
@@ -1,24 +1,25 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
 public class ProcessamentoFinanceiro {
-    public BigDecimal processar(String coin) {
-        try {
-            BigDecimal valorConvertido = converterStringParaBigdecimal(coin);
-            BigDecimal taxaCorregatem = new BigDecimal("1.02");
-            return valorConvertido.multiply(taxaCorregatem);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static final BigDecimal TAXA = new BigDecimal("1.02");
 
-    private BigDecimal converterStringParaBigdecimal(String coin) throws ParseException {
-        Locale brasil = new Locale("pt", "BR");
-        DecimalFormat nf = (DecimalFormat) NumberFormat.getInstance(brasil);
-        nf.setParseBigDecimal(true);
-        return (BigDecimal) nf.parse(coin);
+    private static final ThreadLocal<DecimalFormat> THREAD_FORMAT = ThreadLocal.withInitial(() -> {
+        DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(new Locale("pt", "BR"));
+        df.setParseBigDecimal(true);
+        return df;
+    });
+
+    public BigDecimal processar(String value) {
+        try {
+            BigDecimal valor = (BigDecimal) THREAD_FORMAT.get().parse(value);
+            return valor.multiply(TAXA).setScale(2, RoundingMode.HALF_UP);
+        } catch (ParseException e) {
+            throw new RuntimeException("Erro ao processar valor: " + value, e);
+        }
     }
 }
